145475b01ded70949f369241cf5cf8e57dc0a269
mr.midaw <mr.midaw@gmail.com>
2025-03-22 12:19:41 +0300

    prev questions

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,58 +1,69 @@
 import { Ctx, Message, On, Start, Update } from "nestjs-telegraf";
 import { Scenes, Telegraf } from "telegraf";
-import Gemini, { ChatAskOptions } from "gemini-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { env } from "process";
 
 type ContextType = Scenes.SceneContext;
 
 @Update()
 export class TelegramService extends Telegraf<ContextType> {
-    accessTgUserName = env.FIRST_ACCESS_RIGHTS_USER_NAME;
-    accessSecondTgUserName = env.SECOND_ACCESS_RIGHTS_USER_NAME;
+    accessTgUserId = env.FIRST_ACCESS_RIGHTS_USER_ID;
+    accessSecondTgUserId = env.SECOND_ACCESS_RIGHTS_USER_ID;
     forbiddingString = "Hi, this bot is not for you...";
-    yourTgString = "Your telegram username";
-    previousAnswers: Partial<ChatAskOptions<"text">>;
+    yourTgAnswer = "Your telegram ID";
+    errorResponse = "Something went wrong. Try again later";
 
     @Start()
     async onStart(@Ctx() ctx: ContextType) {
-        const userName = ctx.from?.username;
+        const userId = String(ctx.from?.id);
 
-        if (userName !== this.accessTgUserName && userName !== this.accessSecondTgUserName) {
-            await ctx.reply(`${this.yourTgString}: ${userName}`);
-            await ctx.reply(this.forbiddingString);
-        }
-
-        await ctx.reply(`Привет в чате mrmidaw с GeminiGPT!`);
-        await ctx.reply(`${this.yourTgString}: ${userName}`);
-    }
-
-    @On("text")
-    async onMessage(@Message("text") text: string, @Ctx() ctx: ContextType) {
-        const userName = ctx.from?.username;
-
-        if (userName !== this.accessTgUserName && userName !== this.accessSecondTgUserName) {
-            await ctx.reply(`${this.yourTgString}: ${userName}`);
+        if (userId !== this.accessTgUserId && userId !== this.accessSecondTgUserId) {
+            await ctx.reply(`${this.yourTgAnswer}: ${userId}`);
             await ctx.reply(this.forbiddingString);
             return;
         }
 
-        ctx.reply("Пожалуйста, подождите немного, пока чат-бот обрабатывает ваш запрос . . .");
+        await ctx.reply(`Привет в чате mrmidaw с GeminiGPT!`);
+        await ctx.reply(`${this.yourTgAnswer}: ${userId}`);
+    }
 
-        const gemini = new Gemini(env.GPT_API ?? "");
+    @On("text")
+    async onMessage(@Message("text") text: string, @Ctx() ctx: ContextType) {
+        const userId = String(ctx.from?.id);
 
-        const chat = gemini.createChat();
+        if (userId !== this.accessTgUserId && userId !== this.accessSecondTgUserId) {
+            await ctx.reply(`${this.yourTgAnswer}: ${userId}`);
+            await ctx.reply(this.forbiddingString);
+            return;
+        }
 
-        const answer = await chat.ask(text);
+        ctx.reply(`Пожалуйста, подождите немного, пока чат-бот обрабатывает ваш запрос...`);
 
-        return answer;
+        let prevQuestions: string = "";
+
+        const genAI = new GoogleGenerativeAI(env.GPT_API ?? "");
+        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+        const requestWithPrevValue = prevQuestions + text;
+
+        const result = await model.generateContent(requestWithPrevValue);
+        const aiResponse = result.response.text() ?? this.errorResponse;
+
+        const saveQuestionAndResult = `Вопрос: ${text} - Ответ: ${aiResponse},`;
+
+        prevQuestions += saveQuestionAndResult;
+
+        console.log("🚀 ~ TelegramService ~ onMessage ~ prevQuestions>>>", prevQuestions);
+
+        return aiResponse;
     }
 
     @On("voice")
     async onVoiceMessage(@Ctx() ctx: ContextType) {
-        const userName = ctx.from?.username;
+        const userId = String(ctx.from?.id);
 
-        if (userName !== this.accessTgUserName && userName !== this.accessSecondTgUserName) {
-            await ctx.reply(`${this.yourTgString}: ${userName}`);
+        if (userId !== this.accessTgUserId && userId !== this.accessSecondTgUserId) {
+            await ctx.reply(`${this.yourTgAnswer}: ${userId}`);
             await ctx.reply(this.forbiddingString);
             return;
         }
